7db5282fc81fc7691ac7eac72bbbf27db3cf7f43
root <NeuronRain Commits>
2023-04-28 17:31:27 +0530

    Updated SSL login script

--- GitHubSSL.sh
+++ GitHubSSL.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo $SSL_AUTH_SOCK
-git remote set-url origin git@github.com:shrinivaasanka/virgo64-github-code.git 
+git remote set-url origin git@github.com:shrinivaasanka/virgo64-linux-github-code.git 
 eval "$(ssh-agent -s)"
 ssh-add /root/.ssh/id_ed25519_gitlab
 ssh git@github.com
